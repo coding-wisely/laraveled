1ef6f75878a224c3159fcd7aac269725e6ac846b
Vladimir Nikolic <codingwisely@Vladimirs-MacBook-Pro-2.local>
2025-01-12 10:51:24 +0700

    Just adding laraveled into my local dictionary of PhpStorm

--- app/Livewire/CreateProject.php
+++ app/Livewire/CreateProject.php
@@ -2,11 +2,14 @@
 
 namespace App\Livewire;
 
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Foundation\Application;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class CreateProject extends Component
 {
-    public function render()
+    public function render(): Application|Factory|\Illuminate\Contracts\View\View|View
     {
         return view('livewire.create-project');
     }
