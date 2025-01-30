31213e252385fe36dab2f24d58149c9e7db482c2
Vladimir Nikolic <vladimir@codingwisely.com>
2025-01-30 10:42:39 +0100

    WIP

--- resources/views/components/project-card.blade.php
+++ resources/views/components/project-card.blade.php
@@ -6,7 +6,13 @@
 <flux:card>
     <div class="flex flex-col h-full">
         <div class="flex flex-col items-start justify-start gap-1 space-y-1">
-            <flux:heading>{{ $project->title }}</flux:heading>
+            <flux:heading>
+                <flux:link
+                    wire:navigate.hover
+                    href="{{ route('projects.show', $project->uuid) }}">
+                    {{ $project->title }}
+                </flux:link>
+            </flux:heading>
             <x-average-rating
                 :average-rating="$project->ratings()->avg('rating') ?? 0"
                 :total-ratings="$project->ratings()->count()"
@@ -24,8 +30,8 @@
         </div>
         <!-- description -->
         <div class="flex-1">
-            <flux:subheading
-                size="sm">{!!  $project->short_description??Str::limit($project->description, 250) !!}</flux:subheading>
+            <flux:subheading size="sm">
+                {{  $project->short_description }}</flux:subheading>
         </div>
         @can('view-stats', $project)
             <div class="flex justify-between items-center mt-4">
@@ -47,8 +53,6 @@
                     <flux:link>{{ $project->user->name }}</flux:link>
                 </flux:subheading>
             @endif
-            <flux:button size="xs" icon-trailing="eye" href="{{ route('projects.show', $project->uuid) }}">
-            </flux:button>
         </div>
 
     </div>
