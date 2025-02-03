2320d72c5262b4d5118c4e555f10c1e52542fc02
Namrata199 <namru.mail@gmail.com>
2025-02-03 12:08:35 +0545

    layout like my projects

--- resources/views/livewire/projects/index.blade.php
+++ resources/views/livewire/projects/index.blade.php
@@ -28,7 +28,7 @@
         </flux:select>
     </div>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 mt-6">
         @forelse ($projects as $project)
                 <x-project-card :project="$project" :show-author="true"/>
         @empty
