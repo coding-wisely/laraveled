61830dffd5703d4bed69382066cc09a0e20b1830
Vladimir Nikolic <vladimir@codingwisely.com>
2025-03-04 03:40:27 +0100

    add random order to featured projects retrieval and update dependencies

--- app/Livewire/FeaturedProjectsCarousel.php
+++ app/Livewire/FeaturedProjectsCarousel.php
@@ -26,6 +26,7 @@ class FeaturedProjectsCarousel extends Component
     {
         $featured = Project::with('tags', 'media')
             ->where('is_featured', true)
+            ->inRandomOrder()
             ->get();
 
         if ($featured->isEmpty()) {
